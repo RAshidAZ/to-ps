cff9bedb3a3ddfd2e31c4256014226b751cd7586
MD RASHID ALI <rashid.zeyad2u@gmail.com>
2023-04-06 14:40:24 +0530

    404 and 500 handled in app.js

--- app.js
+++ app.js
@@ -9,4 +9,28 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 
+const auth = require('./routes/auth');
+app.use('/auth', auth);
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals,
+    let errMessage = err.message;
+
+    // send the error reponse
+    res.status(err.status || 500);
+    res.send({
+        success: false,
+        message: errMessage,
+    });
+});
+
 module.exports = app;
